6a78e5333aadf2bb38f9f0f5a3ab943086a5d94e
Erick Boch <erickboch67@gmail.com>
2022-04-30 12:42:07 -0600

    v1.0 mensajes de alerta

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask, redirect, session, url_for
+from tkinter.tix import Tree
+from flask import Flask, redirect, session, url_for,flash
 from flask import render_template, request
 from flask import Response
 from flaskext.mysql import MySQL
@@ -55,7 +56,7 @@ def camera():
 #metodo para validad los datos de entrada del login
 @app.route('/ingresar', methods=['POST', 'GET'])
 def ingresar():
-    msg = ''
+   
     if request.method == 'POST':
         user = request.form['txtUsuario']
         password = request.form['txtPass']
@@ -68,8 +69,8 @@ def ingresar():
             session['username'] = record[1]
             return redirect(url_for('dash'))
         else:
-            msg = '¡Contraseña/Usuario Incorrecto, Intente de Nuevo!'
-    return render_template('login/login.html', msg=msg )
+            flash('!Error. Usuario o contraseña incorrectos')
+    return render_template('login/login.html')
 
 #metodo para cerrar sesion
 @app.route('/logout')
@@ -100,6 +101,7 @@ def storage():
     Cursor = conn.cursor()
     Cursor.execute(sql,datos)
     conn.commit()
+    flash('!Error. Usuario o contraseña incorrectos')
     return render_template('empleados/index.html')
 
 # funcion para Actualizar un usuario    
@@ -110,7 +112,6 @@ def editarEmpleado(id):
     Cursor.execute("SELECT * FROM EMPLEADO WHERE id_emp=%s",(id));
     empleado = Cursor.fetchall();
     print(empleado);
-    
     return render_template('empleados/edit.html',empleado=empleado);
 #funcion para eliminar un usuario
 @app.route("/delete/<int:id>")
@@ -124,7 +125,7 @@ def eliminarUsuario(id):
 
 #metodo para confirmar la edicion de un numero
 @app.route('/edit', methods=['POST'])
-def editarEmpleados():
+def editarEmpleados(): 
     _id = request.form['txtId'];
     nombre = request.form['txtNombre'];
     correo= request.form['txtCorreo'];
@@ -143,6 +144,7 @@ def editarEmpleados():
     Cursor = conn.cursor();
     Cursor.execute(sql,datos);
     conn.commit();
+    flash('!Error. Usuario o contraseña incorrectos');
     return redirect('/list');
 
 #funcion para generar la estrucutra del reconocimiento facial
@@ -166,10 +168,7 @@ def reconocimiento():
    return Response(generate(),mimetype="multipart/x-mixed-replace; boundary=frame")
  # enlace para instalar open cv https://pypi.org/project/opencv-contrib-python/
 
-
 if '__main__':
     app.run(debug=False)
-
-
 cap.release()
   
