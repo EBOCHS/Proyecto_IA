c0b28574875f247428f2d834ae244d6f6710905b
Josue Bor <josh03@gmail.com>
2022-05-21 10:58:51 -0600

    alerta y activacion del actuador

--- static/js/clasificador.js
+++ static/js/clasificador.js
@@ -2,6 +2,10 @@
 let clasificador;
 let video;
 
+let estado;
+let probabilidad;
+
+
 function setup() {
     noCanvas();
     // Inicializa la camara
@@ -15,6 +19,7 @@ function setup() {
     contenedorCamara.appendChild(camaraIA);
 }
     
+
 
 //Cuando el modelo este listo
 function modeloListo() {
@@ -44,6 +49,8 @@ function tomaResultado(err, resultado) {
             "thick solid #00f02b";
         select("#resultado").html("Naranja en Buen Estado");
         select("#probabilidad").html(nf(resultado[0].probability, 0, 2));
+        
+       
     } else if (
         resultado[0].className == "orange" &&
         nf(resultado[0].probability, 0, 2) < "0.60"
@@ -52,11 +59,17 @@ function tomaResultado(err, resultado) {
             "thick solid #dc143c";
         select("#resultado").html("Naranja en Mal estado");
         select("#probabilidad").html(nf(resultado[0].probability, 0, 2));
+        estado='Naranja en Mal estado'
+        probabilidad=nf(resultado[0].probability, 0, 2);
+        axios.post('/insertar-reconocimiento-fruta',{estado:estado, probabilidad:probabilidad        
+        }).then(res=>{
+            console.log(res.data)
+        })
     } else {
         document.getElementById("video").style.border =
             "thick solid #1a3bd6";
         select("#resultado").html("Identificando...");
-        select("#probabilidad").html("");
+        select("#probabilidad").html(""); 
     }
     // Llamamos de nuevo al clasificador
     
